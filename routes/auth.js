f93faf9e2f95496370590b16650d33a1649bedd2
nocopoco <tan_jian_xi_nicholas@hotmail.com>
2020-12-20 22:58:03 +0800

    cookie

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const UserModel = require('../models/UserModel');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const checkToken = require('../checkToken');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -7,7 +12,32 @@ router.get('/', (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-  res.json(req.body);
+  const newUser = new UserModel(req.body);
+  try {
+    await newUser.save();
+    res.json(newUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.body.id);
+    if (user) {
+      //create JWT token
+      const token = jwt.sign({ user: user.name }, config.get('secretToken'));
+      //save token in cookie
+      res.cookie('authcookie', token, { maxAge: 900000000, httpOnly: true });
+      res.json(user);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/getmoney', checkToken, (req, res) => {
+  res.json('No Prob');
 });
 
 module.exports = router;
