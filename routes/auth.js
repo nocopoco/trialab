6abf185ab538ab9c62d1ef9d9a215942edc9ae64
nocopoco <tan_jian_xi_nicholas@hotmail.com>
2020-12-20 22:29:05 +0800

    TRA

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const UserModel = require('../models/UserModel');
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('GET AUTH');
 });
 
-router.post('/', (req, res) => {
-  res.send(req.body);
+router.post('/register', async (req, res) => {
+  res.json(req.body);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const config = require('config');
 const app = express();
 const port = process.env.PORT || 8888;
 
 // bodyParser to read req.body (json)
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 //Connect to mongoDB ATLAS
 mongoose.connect(config.get('connection'), {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
-/*
 app.get('/', (req, res) => {
   res.send('Hello AB');
 });
-*/
 
 //Our routes
 app.use('/api/auth', require('./routes/auth'));
